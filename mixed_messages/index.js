e398059f653c5a3200983b1431b229c5a177ed1d
Joel Acosta <tec.joelacosta@gmail.com>
2021-06-24 14:57:32 -0300

    reformed num variable to make it better

--- mixed_messages/index.js
+++ mixed_messages/index.js
@@ -1,9 +1,12 @@
 const mixed_messages = () =>{
-    let num = ()=> {return Math.floor(Math.random()*3)};
+    let num = (array)=> {return Math.floor(Math.random()*array.length)}; // it will be used to randomize the index of each list
+
     const sign = ['Aries', 'Tauro', 'Cancer'];
     const personalaty = ['Practical and Creative', 'emotional and impulsive', 'racional and cold'];
     const luck = ['n Ominous day, you should avoid going out', ' Good day to go out and make friends', ' normal day, dont expect many events '];
-    let message = `Your sign is ${sign[num()]} , you could say that you are ${personalaty[num()]}. Today is a${luck[num()]}.`;
+
+    let message = `Your sign is ${sign[num(sign)]} , you could say that you are ${personalaty[num(personalaty)]}. Today is a${luck[num(luck)]}.`;
+
     console.log(message);
 };
 
