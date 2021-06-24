90f2457f6f5ef763e634c309b052ec81c09dd786
Joel Acosta <tec.joelacosta@gmail.com>
2021-06-24 14:42:31 -0300

    tested and finished... i hope

--- mixed_messages/index.js
+++ mixed_messages/index.js
@@ -1,10 +1,10 @@
 const mixed_messages = () =>{
-    let num = Math.floor(Math.random()*2);
+    let num = ()=> {return Math.floor(Math.random()*3)};
     const sign = ['Aries', 'Tauro', 'Cancer'];
     const personalaty = ['Practical and Creative', 'emotional and impulsive', 'racional and cold'];
     const luck = ['n Ominous day, you should avoid going out', ' Good day to go out and make friends', ' normal day, dont expect many events '];
-    let message = `Your sign is ${sign[num]} , you could say that you are ${personalaty[num]}. Today is a${luck[num]}.`;
-    return message;
+    let message = `Your sign is ${sign[num()]} , you could say that you are ${personalaty[num()]}. Today is a${luck[num()]}.`;
+    console.log(message);
 };
 
-console.log(mixed_messages());
+mixed_messages();
